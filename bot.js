ca6a9ae1c7f890aa3da7b452d3140962b617a6be
YASSIN974 <44032576+YASSIN974@users.noreply.github.com>
2018-11-21 17:44:44 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+client.on("ready", () => {
+client.user.setPresence({
+  status: 'dnd',
+  game: { 
+     type: 0,
+     name: 'Surprise Mother Fucker :) ',
+     details: `I'm : KBOOSH - Developer and Programmer`,
+     url: 'http://twitch.tv/Streammingg',
+     state: `إنْ لَمْ تَجِد لكْ حآقدْ إعلمْ أنْك إنسآن فآشِلْ`,
+    application_id: '281376075802476544',
+     assets: {
+        small_image: `366835431037337600`,
+        small_text: ' Take This ! ' ,
+        large_image: `370451271133429760`,
+        large_text: `  WANTED }
+
+  }
+    });
+});
